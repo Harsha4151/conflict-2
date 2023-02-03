97eb3fb6dc322d6d01cf9e3eaab916a6246f3b2f
Harsha4151 <hkamble4151@gmail.com>
2023-02-03 21:29:18 +0530

    changes by harsha

---
+++ src/main/java/com/BikkadIT/Conflict2Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conflict2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Conflict2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/Demoo.java
@@ -0,0 +1,8 @@
+package com.BikkadIT;
+
+public class Demoo {
+	public static void main(String[] args) {
+		System.out.println("hii");
+	}
+
+}
